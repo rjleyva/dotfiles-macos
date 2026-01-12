c8644d4a2f15e7ac4c4778ac5adfa2c2beb12333
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2026-01-11 20:59:23 -0500

    chore(nvim): enable node and python3 in options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -70,9 +70,7 @@ function M.misc()
 
 	-- Disable unused language providers to clean up :checkhealth
 	vim.g.loaded_ruby_provider = 0
-	vim.g.loaded_node_provider = 0
 	vim.g.loaded_perl_provider = 0
-	vim.g.loaded_python3_provider = 0
 end
 
 function M.setup()
